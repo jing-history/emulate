b143ec5e8d85615588835bdbfb7ea6a0e1a5febb
jingzing <louiswang0898@gmail.com>
2016-06-21 18:26:38 +0800

    多线程去获取数据

--- emulate-spring-redis/src/test/java/tk/jingzing/test/InitDataToDataBase.java
+++ emulate-spring-redis/src/test/java/tk/jingzing/test/InitDataToDataBase.java
@@ -1,10 +1,24 @@
 package tk.jingzing.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.util.concurrent.*;
 
 /**
  * @ClassName:InitDataToDataBase.java
@@ -14,5 +28,41 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring-content.xml"})
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class })
 public class InitDataToDataBase extends AbstractTransactionalJUnit4SpringContextTests {
+    private static final Logger logger = LoggerFactory.getLogger(InitDataToDataBase.class);
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    // 存放url 和title
+    public ConcurrentHashMap<String, String> currentHashMap = new ConcurrentHashMap<String, String>();
+
+    @Test
+    @Transactional
+    public void test() throws IOException {
+        final Session session = sessionFactory.getCurrentSession();
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        try{
+            for (int i = 0; i < 20; i++) {// 总共20也 开启20个线程去爬去链接
+                pool.execute(new PutArticelUrlByPage(currentHashMap, i));
+            }
+            pool.shutdown();
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);// 线程池里面线程所有执行完之后执行下面
+
+            // 处理map里面的链接，保存到数据库。大概有1123个url
+            System.out.println("map大小：" + currentHashMap.size());
+            long beginTime = System.currentTimeMillis();
+
+            // 【2】多线程插入、CountDownLatch是用来解决防止session关闭的问题
+            CountDownLatch latch = new CountDownLatch(currentHashMap.size());
+        //    List<Note> listNote = new StoreToDataBaseByThread(currentHashMap, session, latch).insertToDatabase();
+            latch.await();
+
+        }catch (Exception e){
+            logger.error("" + e.getLocalizedMessage());
+        }
+    }
 }
---
+++ emulate-springboot-basis/src/test/java/tk.jingzing/CommTest.java
@@ -0,0 +1,12 @@
+package tk.jingzing;
+
+/**
+ * Created by Louis Wang on 2016/6/17.
+ */
+
+public class CommTest {
+    public static void main(String[] args) {
+        String ii = null;
+        Integer.parseInt(ii);
+    }
+}
