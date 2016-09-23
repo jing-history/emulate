544ccf3489cdb000dede433a68d9d4f812011d2e
jingzing <louiswang0898@gmail.com>
2016-09-23 22:49:03 +0800

    book jersey chapter2 put method is ok

--- emulate-java-restful-web/jax-guide-ch02/src/test/java/tk/jingzing/jax/annotation/method/PutTest.java
+++ emulate-java-restful-web/jax-guide-ch02/src/test/java/tk/jingzing/jax/annotation/method/PutTest.java
@@ -1,12 +1,15 @@
 package tk.jingzing.jax.annotation.method;
 
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Assert;
 import org.junit.Test;
 import tk.jingzing.jax.domain.Book;
 
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -14,12 +17,12 @@ public class PutTest extends JerseyTest {
     private final static Logger LOGGER = Logger.getLogger(PutTest.class);
     public static AtomicLong clientBookSequence = new AtomicLong();
 
-    /*@Override
+    @Override
     protected Application configure() {
         enable(TestProperties.LOG_TRAFFIC);
         enable(TestProperties.DUMP_ENTITY);
         return new ResourceConfig(EBookResourceImpl.class);
-    }*/
+    }
 
     @Test
     public void testNew() {
@@ -29,6 +32,6 @@ public class PutTest extends JerseyTest {
         final Entity<Book> bookEntity = Entity.entity(newBook, contentTypeMediaType);
         final String lastUpdate = target("book").request(acceptMediaType).put(bookEntity, String.class);
         Assert.assertNotNull(lastUpdate);
-        LOGGER.debug(lastUpdate);
+        LOGGER.info("data-->" + lastUpdate);
     }
 }
